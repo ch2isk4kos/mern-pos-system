824b92bbccd5a1a29d7dcab78fcce69645a8f148
ch2isk4kos <ch2isk4kos@gmail.com>
2023-12-03 17:28:53 -0500

    import body-parser and socket.io reqs

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const http = require("http");
+const bodyParser = require("body-parser");
+const socket = require("socket.io");
 const port = 80;
 
 const app = express();
