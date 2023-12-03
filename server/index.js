dfe269226e5f7b0d9e0229397d51049553848548
ch2isk4kos <ch2isk4kos@gmail.com>
2023-12-03 17:52:40 -0500

    add connection to web socket

--- server/index.js
+++ server/index.js
@@ -4,15 +4,15 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 
-const socket = require("socket.io")(server);
+const io = require("socket.io")(server);
 const bodyParser = require("body-parser");
 
 // const PORT = 80;
 const PORT = process.env.PORT;
 
-// const liveCart;
+let liveCart;
 
-console.log(`POS system server running on port: ${PORT}`);
+console.log(`POS system running`);
 
 // MIDDLEWARE
 app.use(bodyParser.json());
@@ -35,9 +35,33 @@ app.all("/*", function (req, res, next) {
   }
 });
 
+// home
 app.get("/", function (req, res) {
   res.send("POS systems a go");
 });
 
 app.use("/api/inventory", require("./api/inventory"));
 app.use("/api/transactions", require("./api/transactions"));
+
+// WEB SOCKET
+io.on("connection", function (socket) {
+  // on page load, show user current cart
+  socket.on("cart-transaction-complete", function () {
+    socket.broadcast.emit("update-live-cart-display", {});
+  });
+  // when client connected, make client update live cart
+  socket.on("live-cart-page-loaded", function () {
+    socket.emit("update-live-cart-display", liveCart);
+  });
+  // when the cart data is updated by the POS
+  socket.emit("update-live-cart-display", liveCart);
+  // keep track of it
+  socket.on("update-live-cart", function (cartData) {
+    liveCart = cartData; // broadcast updated live cart to all websocket clients
+    socket.broadcast.emit("update-live-cart-display", liveCart);
+  });
+});
+
+server.listen(port, () =>
+  console.log(`POS system server listening on port: ${PORT}`)
+);
