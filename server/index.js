daf9de165310a5c9d188e79d7e3c3f136a705a05
ch2isk4kos <ch2isk4kos@gmail.com>
2023-12-03 17:44:55 -0500

    add cors configuration and index route(s)

--- server/index.js
+++ server/index.js
@@ -14,6 +14,30 @@ const PORT = process.env.PORT;
 
 console.log(`POS system server running on port: ${PORT}`);
 
-// middleware
+// MIDDLEWARE
 app.use(bodyParser.json());
 app.use(bodyParser.urlEncoded({ extended: false }));
+
+// ROUTES
+app.all("/*", function (req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  // Set custom headers for CORS
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-type,Accept,X-Access-Token,X-Key"
+  );
+  if (req.method == "OPTIONS") {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
+app.get("/", function (req, res) {
+  res.send("POS systems a go");
+});
+
+app.use("/api/inventory", require("./api/inventory"));
+app.use("/api/transactions", require("./api/transactions"));
